220d968cd038ea99dcd3c2a3b5ff925d38803a73
SkeleDrook <hulik.ondrej@gmail.com>
2024-08-04 02:41:07 +0000

    Three out of 5 done

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -1,19 +1,35 @@
 def display_current_inventory(inventory_list)
   # use #each to iterate through each item of the inventory_list (a hash)
   # use puts to output each list item "<key>, quantity: <value>" to console
+  inventory_list.each do |key, value|
+    puts "#{key}, quantity: #{value}"
+  end
 end
 
+require 'pry-byebug'
 def display_guess_order(guesses)
   # use #each_with_index to iterate through each item of the guesses (an array)
   # use puts to output each list item "Guess #<number> is <item>" to console
   # hint: the number should start with 1
+  guesses.each_with_index do |item, number| 
+    puts "Guess ##{number + 1} is #{item}"
+    # binding.pry
+  end
 end
-
+# display_guess_order(['cookies', 'cake', 'ice cream'])
 def find_absolute_values(numbers)
   # use #map to iterate through each item of the numbers (an array)
   # return an array of absolute values of each number
+  numbers.map {|number| number.abs}
+  # numbers.map do |number|
+  #   if number < 0
+  #     number * -1
+  #   else
+  #     number
+  #   end
+  # end
 end
-
+find_absolute_values([1,4,-4,6,-9])
 def find_low_inventory(inventory_list)
   # use #select to iterate through each item of the inventory_list (a hash)
   # return a hash of items with values less than 4
